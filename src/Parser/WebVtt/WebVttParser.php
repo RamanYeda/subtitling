6d1da91cc2ec08dacabb61bb128c74a69cb6cdaa
Raman Yeda <grayfox010389@gmail.com>
2016-09-04 17:05:11 +0300

    Basic web vtt parser skeleton.

---
+++ src/Parser/WebVtt/WebVttParser.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Subtitling.
+ *
+ * @author Raman Yeda <grayfox010389@gmail.com>
+ * @copyright 2016 Raman Yeda
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
+namespace Yeda\Subtitling\Parser\WebVtt;
+
+use Yeda\Subtitling\Parser\Parser;
+use Yeda\Subtitling\Parser\AbstractParserSubject;
+
+class WebVttParser extends AbstractParserSubject implements Parser
+{
+    public function parse(InputSource $source)
+    {
+        // Parsing Web VTT realization here.
+    }
+}
---
+++ src/Parser/WebVtt/WebVttParserState.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Subtitling.
+ *
+ * @author Raman Yeda <grayfox010389@gmail.com>
+ * @copyright 2016 Raman Yeda
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
+namespace Yeda\Subtitling\Parser\WebVtt;
+
+interface WebVttParserState
+{
+    // Parser state transition methods declaration.
+}
